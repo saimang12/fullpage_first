7d6050b6cfd93d1b482e5310af5273d70ead0337
saimang12@naver.com <saimang12@naver.com>
2023-03-13 17:38:11 +0900

    첫 풀페이지

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.main').fullpage({
+    anchors: ['sec01', 'sec02', 'sec03', 'sec04', 'sec05'],
+    afterLoad: function (anchorLink, index) {
+        console.log(anchorLink, index);
+        $('#header nav>ul>li').eq(index - 1).addClass('on')
+        sibling().removeClass('on');
+        $('.section').eq(index - 1).addClass('on')
+        sibling().removeClass('on');
+    }
+
+
+});
+
+
+$('#header nav>ul>li').on('click', function () {
+    var idx = $(this).index();
+    $(this).addClass('on').siblings().removeClass('on')
+})
